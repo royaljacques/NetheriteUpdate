9bdcf3337b1425fbaebe83b8f32843574a570033
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2020-06-28 17:08:56 +0200

    ajouts de 2 ou trois fonctionnalitée

--- NetherItems/src/royal/NetherItems/Main.php
+++ NetherItems/src/royal/NetherItems/Main.php
@@ -24,16 +24,18 @@ public function onEnable(){
     $this->getLogger()->info( "§aPlugin Chargé avec succès !");
     ItemFactory::registerItem(new NetheriteBoots(),true);
     ItemFactory::registerItem(new NetheriteChestplate(),true);
+    ItemFactory::registerItem(new Item(752, 0, "Netherite Scrap"));
     ItemFactory::registerItem(new NetheriteHelmet(),true);
-    ItemFactory::registerItem(new NetheriteLeggings(),true);
-    ItemFactory::registerItem(new NetheriteSword(),true);
+	ItemFactory::registerItem(new NetheriteLeggings(),true);
+	ItemFactory::registerItem(new NetheriteSword(),true);
+    ItemFactory::registerItem(new Item(736, 0, "Honeycomp"));
     ItemFactory::registerItem(new NetheritePickaxe(),true);
     ItemFactory::registerItem(new NetheriteAxe(),true);
     ItemFactory::registerItem(new NetheriteShovel(),true);
     ItemFactory::registerItem(new NetheriteHoe(),true);
     ItemFactory::registerItem(new Item(742, 0, "Netherite Ingot"));
-    ItemFactory::registerItem(new Item(752, 0, "Netherite Scrap"));
-    ItemFactory::registerItem(new Item(736, 0, "Honeycomp"));
+    ItemFactory::registerItem(new SuspiciousStew(), true);
+    ItemFactory::registerItem(new HoneyBottle(), true);
     Item::initCreativeItems();
 
     }
